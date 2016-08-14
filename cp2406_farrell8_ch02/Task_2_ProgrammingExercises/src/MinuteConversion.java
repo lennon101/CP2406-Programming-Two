84752b81cd0c109ac426b90a83669bd12fa17431
Dane Lennon <dane.lennon101@gmail.com>
2016-08-14 19:30:00 +1000

    add MinuteConversion Class

---
+++ cp2406_farrell8_ch02/Task_2_ProgrammingExercises/src/MinuteConversion.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+/**
+ * Created by danelennon on 14/08/2016.
+ */
+public class MinuteConversion {
+    public static void main(String[] args) {
+        boolean valid = false;
+        int numMinutes = 0;
+
+        Scanner input = new Scanner(System.in);
+
+        while (numMinutes<=0){
+            System.out.println("invalid input, try again...");
+            valid = false;
+            while (!valid) {
+                System.out.print("Enter a value in nautical miles to convert: ");
+                try {
+                    numMinutes = Integer.parseInt(input.next());
+                    valid = true;
+                } catch (NumberFormatException e) {
+                    System.out.println("invalid input, try again...");
+                    System.out.print("Enter a value in nautical miles to convert: ");
+                }
+            }
+        }
+
+        double numHours = numMinutes/60;
+        double numDays = numHours/24;
+
+        System.out.println(numMinutes + " minutes equats to: " + String.format("%.2f", numHours)  + " hrs OR " + String.format("%.2f", numDays) + " days");
+    }
+}
