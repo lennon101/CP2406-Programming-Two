fcf5290031f9a707aef9be89ac2ff9f4c723e1dd
Dane Lennon <dane.lennon101@gmail.com>
2016-08-15 10:08:25 +1000

    complete debug exercises

--- cp2406_farrell8_ch03/DebuggingExercises/DebugThree3.java
+++ cp2406_farrell8_ch03/DebuggingExercises/DebugThree3.java
@@ -2,22 +2,20 @@
 import java.util.Scanner;
 public class DebugThree3
 {
-   public static void main(String args[])
-   {
-      String name;
-      getName();
+   public static void main(String args[]) {
+      String name = getName();
       displayGreeting(name);           
    }
-   public static String getName()
-   {
+
+   public static String getName() {
       String name;
       Scanner input = new Scanner(System.in);
-      System.out.print(Enter name );
-      name = input.nextInt();
+      System.out.print("Enter name: ");
+      name = input.next();
       return name;
    }
-   public static void displayGreeting()
-   {
+
+   public static void displayGreeting(String name) {
       System.out.println("Hello, " + name + "!");
    }
 }
--- cp2406_farrell8_ch03/DebuggingExercises/DebugThree4.java
+++ cp2406_farrell8_ch03/DebuggingExercises/DebugThree4.java
@@ -3,16 +3,15 @@ public class DebugThree4
 {
    public static void main(String args[])
    {
-      int myCredits = 13
-      int yourCredits = 17
-      double rate = 75.84
+      int myCredits = 13;
+      int yourCredits = 17;
+      double rate = 75.84;
       System.out.println("My tuition:");
       tuitionBill(myCredits, rate);
       System.out.println("Your tuition:");
-      tuitionBill(myCredits, rate);
+      tuitionBill(yourCredits, rate);
    }
-   public static void tuitionBill(double r)
-   {
-      System.out.println("Total due " + (r*c));
+   public static void tuitionBill(int c, double r) {
+      System.out.println("Total due " + String.format("%.2f",r*c));
    }
 }
