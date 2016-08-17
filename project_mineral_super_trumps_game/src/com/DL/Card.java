ccb3581c464ec4b15ed978918e8800e660fe4bd2
Dane Lennon <dane.lennon101@gmail.com>
2016-08-18 01:03:53 +1000

    initial commit for assignment

---
+++ project_mineral_super_trumps_game/src/com/DL/Card.java
@@ -0,0 +1,23 @@
+package com.DL;
+
+
+/**
+ * Created by danelennon on 18/08/2016.
+ */
+public class Card {
+    private String fileName:
+    private String imageName;
+    private String cardType;
+    private String title;
+    private String chemistry;
+    private String classification;
+    private String crystalSystem;
+    private String[] occrance;
+    private int hardness;
+    private int specGravity;
+    private String cleavage;
+    private String crystalAbundance;
+    private String economicValue;
+
+
+}
---
+++ project_mineral_super_trumps_game/src/com/DL/Main.java
@@ -0,0 +1,8 @@
+package com.DL;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
