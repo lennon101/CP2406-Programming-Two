e49b379f919ed5a802645bbceed663cdebfed526
Dane Lennon <dane.lennon101@gmail.com>
2016-08-22 09:58:04 +1000

    complete chap 5 debug exercsises

--- cp2406_farrell8_ch05/DebuggingExercises/DebugFive4.java
+++ cp2406_farrell8_ch05/DebuggingExercises/DebugFive4.java
@@ -3,30 +3,27 @@
 import javax.swing.*;
 public class DebugFive4
 {
-   public static void main (String args[]) 
-   {
-      int one, two, three, four;
-      String str, output;
-      str = JOptionPane.showInputDialog(null,"Enter an integer");
-      one = Integer.parseInt();
-      str = JOptionPane.showInputDialog(null,"Enter an integer");
-      two = Integer.parseInt();
-      str = JOptionPane.showInputDialog(null,"Enter an integer");
-      two = Integer.parseInt();
-      str = JOptionPane.showInputDialog(null,"Enter an integer");
-      four = Integer.parseInt();
-      if(one > two & one >= three && one < four)
-         output = "Highest is " + one;
-      else
-         if(two > one || two > three && two > four)
-            output = "Highest is " + two;
-         else
-           if(three > one && three > two || three == four)
-              output = "Highest is " + three;
-           else
-              output = "Highest is " + three;
-      JOptionPane.showMessageDialog(null, output);
-   }
+    public static void main (String args[])
+    {
+
+        int one, two, three, four;
+        String str, output;
+        one = Integer.parseInt(JOptionPane.showInputDialog(null,"Enter an integer"));
+        two = Integer.parseInt(JOptionPane.showInputDialog(null,"Enter an integer"));
+        three = Integer.parseInt(JOptionPane.showInputDialog(null,"Enter an integer"));
+        four = Integer.parseInt(JOptionPane.showInputDialog(null,"Enter an integer"));
+
+        if(one > two && one > three && one > four) {
+            output = "Highest is " + one + " stored in var one";
+        }else if(two > one && two > three && two > four) {
+            output = "Highest is " + two + " stored in var two";
+        }else if(three > one && three > two && three > four) {
+            output = "Highest is " + three + " stored in var three";
+        }else {
+            output = "Highest is " + four + " stored in var four";
+        }
+        JOptionPane.showMessageDialog(null, output);
+    }
 }
 
 
